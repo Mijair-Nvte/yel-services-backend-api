e1586dcc8dba17f52472b8b467fcd1d7b1e83e4b
Mijair-Nvte <mijairnvte@gmail.com>
2026-04-22 14:55:07 -0600

    se agrego migracion parche

--- database/migrations/2026_04_22_204845_alter_spatie_tables_add_team_id.php
+++ database/migrations/2026_04_22_204845_alter_spatie_tables_add_team_id.php
@@ -10,36 +10,24 @@ return new class extends Migration
     {
         $teamKey = config('permission.column_names.team_foreign_key', 'org_company_id');
 
-        // 1. Agregamos la columna a 'roles'
+        // 1. Agregamos a 'roles'
         Schema::table('roles', function (Blueprint $table) use ($teamKey) {
             if (!Schema::hasColumn('roles', $teamKey)) {
                 $table->unsignedBigInteger($teamKey)->nullable()->after('id');
             }
         });
 
-        // 2. Agregamos a 'model_has_roles' y actualizamos las llaves primarias
+        // 2. Agregamos a 'model_has_roles'
         Schema::table('model_has_roles', function (Blueprint $table) use ($teamKey) {
             if (!Schema::hasColumn('model_has_roles', $teamKey)) {
                 $table->unsignedBigInteger($teamKey)->default(1); 
-                
-                $table->dropPrimary();
-                $table->primary(
-                    [$teamKey, 'role_id', 'model_id', 'model_type'], 
-                    'model_has_roles_team_primary'
-                );
             }
         });
 
-        // 3. Agregamos a 'model_has_permissions' y actualizamos llaves primarias
+        // 3. Agregamos a 'model_has_permissions'
         Schema::table('model_has_permissions', function (Blueprint $table) use ($teamKey) {
             if (!Schema::hasColumn('model_has_permissions', $teamKey)) {
                 $table->unsignedBigInteger($teamKey)->default(1);
-                
-                $table->dropPrimary();
-                $table->primary(
-                    [$teamKey, 'permission_id', 'model_id', 'model_type'], 
-                    'model_has_permissions_team_primary'
-                );
             }
         });
     }
